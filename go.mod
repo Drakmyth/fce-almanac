258faa18acdca6b4430132d161da3c27461d1aa6
Drakmyth <shaun.hamman@gmail.com>
2023-10-08 13:42:13 -0700

    Implement config and cli support.

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/Drakmyth/fce-almanac
 
 go 1.21.2
 
-require github.com/jba/muxpatterns v0.3.0
+require (
+	github.com/jba/muxpatterns v0.3.0
+	github.com/peterbourgon/ff/v4 v4.0.0-alpha.4
+)
 
 require golang.org/x/exp v0.0.0-20230519143937-03e91628a987 // indirect
--- server.go
+++ server.go
@@ -1,22 +1,75 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/jba/muxpatterns"
+	"github.com/peterbourgon/ff/v4"
+	"github.com/peterbourgon/ff/v4/ffhelp"
 )
 
 var tmpl *template.Template = nil
+var config string = ""
+var verbose bool = false
+var port int = 8000
 
 func main() {
-	fmt.Println("Hello World!")
+	rootFlags := ff.NewFlagSet("fce-almanac")
+	rootFlags.StringVar(&config, 'c', "config", config, "config file")
+	rootFlags.IntVar(&port, 'p', "port", port, "application port")
+	rootFlags.BoolVar(&verbose, 'v', "verbose", "increase log verbosity")
+	rootCmd := &ff.Command{
+		Name:      "fce-almanac",
+		ShortHelp: "Run almanac backend server",
+		Usage:     "fce-almanac [FLAGS] [SUBCOMMAND]",
+		Flags:     rootFlags,
+		Exec:      runserver,
+	}
 
+	initdbFlags := ff.NewFlagSet("initdb").SetParent(rootFlags)
+	initdbCmd := &ff.Command{
+		Name:      "initdb",
+		Usage:     "fce-almanac initdb <path>",
+		ShortHelp: "Initialize db with FortressCraft Evolved data",
+		Flags:     initdbFlags,
+		Exec:      initdb,
+	}
+	rootCmd.Subcommands = append(rootCmd.Subcommands, initdbCmd)
+
+	err := rootCmd.ParseAndRun(context.Background(), os.Args[1:],
+		ff.WithEnvVarPrefix("FCE"),
+		ff.WithConfigFileFlag("config"),
+		ff.WithConfigFileParser(ff.PlainParser),
+		ff.WithConfigIgnoreUndefinedFlags())
+
+	switch {
+	case errors.Is(err, ff.ErrHelp):
+		fmt.Fprintf(os.Stderr, "%s\n", ffhelp.Command(rootCmd))
+		os.Exit(0)
+	case err != nil:
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func initdb(ctx context.Context, args []string) error {
+	fmt.Fprintf(os.Stdout, "TODO: Initialize db\n")
+	return nil
+}
+
+func runserver(ctx context.Context, args []string) error {
 	mux := muxpatterns.NewServeMux()
 	tmpl, _ = template.ParseGlob("./templates/*.tmpl.html")
 
 	mux.Handle("/", http.FileServer(http.Dir("public")))
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	fmt.Fprintf(os.Stdout, "Applicaton listening on port %d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	log.Fatal(err)
+	return err
 }
